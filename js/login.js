f45cc5083c5d3c11714d793367ed98b5659a2394
Rayhan Kobir <rayhankobir972@gmail.com>
2023-08-14 19:41:23 +0600

    added login page

---
+++ js/login.js
@@ -0,0 +1,22 @@
+//step-1: add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click',function(){
+    // step-2: get the email 
+    //always remember to use .value to get text from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // step-3:get password
+    // 3.a : set id on the html Element
+    // 3.b :get the Element
+    // 3.c:get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    
+    //DO NOT VERIFY email password on the client side
+    //step-4: verify email and password
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        console.log('valid user');
+    }
+    else{
+        console.log('invalid user');
+    }
+})
